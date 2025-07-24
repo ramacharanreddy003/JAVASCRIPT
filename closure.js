7652dc99aafd29d1eb87597d5a8821e287b8f72d
Chinnakambaiah Gari Rama Charan Reddy <ckramcharanreddy03@gmail.com>
2025-07-24 19:07:06 +0530

    Add files via upload

---
+++ closure.js
@@ -0,0 +1,56 @@
+//closure in js
+/*
+function x(){
+    var a=7;
+    function y(){
+        console.log(a);
+    }
+    y();
+}
+x();
+
+function b(){
+    var t=5;
+    function c(){
+        console.log(t);
+    }
+    c();
+}
+b();*/
+/*
+
+function a(){
+    var b=45;
+    function c(){
+        console.log(b);
+    }
+   // var b=89;
+    c();
+}
+a();
+*/
+
+/*
+function ad(){
+    const a=5;
+    const b=6;
+    const c=a+b;
+    function add(){
+        console.log(c);
+    }
+    add();
+}
+ad();
+*/
+
+
+function o(){
+    var b=537;
+    function i(){
+        console.log(b);
+    }
+    
+    i();
+}
+o();
+
