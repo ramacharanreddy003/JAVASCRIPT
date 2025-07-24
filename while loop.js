ecf8a6defe23fd11256570ca270b3bec37a66607
Chinnakambaiah Gari Rama Charan Reddy <ckramcharanreddy03@gmail.com>
2025-07-24 19:04:38 +0530

    Add files via upload

---
+++ while loop.js
@@ -0,0 +1,37 @@
+//while loop
+/*var i=4;
+while(i<=15){
+    //if(i==11){ break;}
+    console.log(i);
+    i++;
+}*/
+
+//do while loop
+/*
+var c=5;
+do{
+    console.log(c);
+    c++;
+}
+while(c<=8);
+*/
+/*
+let j=20;
+do{
+    console.log(j);
+    j++;
+}while(j<=30);
+
+*/
+
+//practical 
+
+
+let n=5;
+let sum=0
+let i=1;
+while(i<=n){
+    sum+=i;
+    i++;
+}
+console.log(n+sum);
