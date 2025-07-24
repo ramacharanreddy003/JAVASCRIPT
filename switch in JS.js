10ada508d2628f6e0421aeef4e25f27063d6aa13
Chinnakambaiah Gari Rama Charan Reddy <ckramcharanreddy03@gmail.com>
2025-07-24 18:45:54 +0530

    Add files via upload

---
+++ switch in JS.js
@@ -0,0 +1,183 @@
+/*var grade="C";
+var result;
+switch(grade){
+    case 'A':
+        result="A Grade";
+        break;
+    case 'B':
+        result="B Grade";
+        break;
+    case 'C':
+        result="C Grade";
+        break;
+    case 'D':
+        result="D Grade";
+        break;
+
+}
+console.log(result);
+*/
+/*
+let n1=75;
+let n2=5;
+let operator="-";
+switch(operator){
+    case "+":
+        console.log(`Result: ${n1+n2}`);
+        break;
+    case "-":
+        console.log(`Result: ${n1-n2}`);
+        break;
+    case "*":
+        console.log(`Result: ${n1*n2}`);
+        break;
+    case "/":
+        console.log(`Result: ${n1/n2}`);
+        break;
+    default:
+        console.log("Invslid opeartor");
+}*/
+/*
+let day=7;
+switch(day){
+    case 1:
+        console.log("Today is Monday");
+        break;
+    case 2:
+        console.log("Today is Tuesday");
+        break;
+    case 3:
+        console.log("Today is Wednesday");
+        break;
+    case 4:
+        console.log("Today is Thursday");
+        break;
+    case 5:
+        console.log("Today is Friday");
+        break;
+    case 6:
+        console.log("Today is Saturday");
+        break;
+    case 7:
+        console.log("Today is Sunday");
+}
+*/
+/*
+let month=8;
+switch(month){
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Febuary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("December");
+        break;
+    default:
+        console.log("Invalid");
+}
+
+let color="orange";
+switch(color){
+    case "red":
+        console.log("Red means Stop");
+        break;
+    case "green":
+        console.log("Green means Go");
+        break;
+    case "orange":
+        console.log("orange means ready to Go");
+        break;
+    default:
+        console.log("Invalid color");
+}
+*/
+/*
+
+//traffiic signals
+let colors="red";
+let action;
+switch(colors){
+    case "red":
+        action="Stop";
+        break;
+    case "green":
+        action="Go";
+        break;
+    case "orange":
+        action="Ready to go";
+        break;
+    default:
+        action="Invalid color";
+    
+}
+console.log(`${action}`);
+*/
+
+//fruit price.
+fruit="orange";
+let action;
+switch(fruit){
+    case "apple":
+        action="Price of apple is R.S:200";
+        break;
+    case "orange":
+        action="Price of orange is R.S:100";
+        break;
+    case "mango":
+        action="Price of mango is R.S:100";
+        break;
+    default:
+        action="Invalid fruit";
+        break;
+}
+console.log(action);
+
+//weather forecast
+/*
+let weather_condition="rainy";
+let action;
+switch(weather_condition){
+    case "sunny":
+        action="Now the weather is as sunny";
+        break;
+    case "rainy":
+        action="Now the weather is as rainy";
+        break;
+    case "cloudy":
+        action="Now the weather is as cloudy";
+        break;
+    default:
+        action="Invalid";
+        break;
+}
+console.log(action);
+*/
